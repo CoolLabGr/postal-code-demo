c6a6c3ab25a763588611cd07da3241254f9e5dc5
CoolLabGr <cool.lab.gr@outlook.com>
2017-10-27 05:29:45 +0300

    mmmmmm

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 ## Postal Code Demo.
 
-* download or clone the repo
-* install express with npm install
-* run the server
-* enjoy the demo
+* try 11364 or 11526
--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const server = express()
-
-server.use(express.static('./public'))
-
-server.listen(8081, () => {
-  console.log('server @ http://localhost:8081')
-})
