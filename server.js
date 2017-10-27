792d9f23e45ae84d072da99de2e3f4606ad9c904
CoolLabGr <cool.lab.gr@outlook.com>
2017-10-27 05:21:31 +0300

    Change xml back to server

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const server = express()
 
 server.use(express.static('./public'))
 
-server.listen(8080, () => {
-  console.log('server @ http://localhost:8080')
+server.listen(8081, () => {
+  console.log('server @ http://localhost:8081')
 })
